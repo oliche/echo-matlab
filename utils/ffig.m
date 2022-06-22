a641ee82028ac287132408745ef19e4cfa60aba2
olivier <olivier.winter@hotmail.fr>
2022-06-22 15:06:54 +0100

    ffig.m figure utility for copying to clipboard

---
+++ utils/ffig.m
@@ -0,0 +1,23 @@
+function fig = ffig(inputArg1,inputArg2)
+% returns a larger figure with white backgroung and a shortcut to copy
+% to clipboard with ctr + shift + c
+
+
+fig = figure('position', [200 200 900 600], 'color', 'w')
+
+set(fig, 'WindowKeyPressFcn', @key_press)
+
+end
+
+
+
+function key_press(fig, evt)
+
+if strcmp(evt.Key,  'c') && ~isempty(evt.Modifier)
+    if all(cellfun(@(x) strcmp(x, 'shift') | strcmp(x, 'control'), evt.Modifier))
+        print(fig, '-clipboard', '-dbitmap')
+    end
+end
+end
+
+
